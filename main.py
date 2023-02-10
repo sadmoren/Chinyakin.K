36ca0bb6b931aec3c79266056226db713e3d51d6
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-02-10 11:46:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+text = 'Hello, Python'
+print (text[0])
+print (text[0:5])
+print (text[4:10])
+print (text[0:14])
+print (text[7:])
+print (text[:5])
+print (text[:])
+print (text[-1])
+print (text[-1:-14])
+print (text[::2])
+print (text[::-1])
+print (text +'Nice to code you')
+print (text[-1])
+
+
+my_list =[True, 786, 3.14, 'text', 70.2]
+second_list =[123, 'text']
+
+print (my_list) 
+print (my_list[0])
+print (my_list[1:3])
+print (my_list[2:]) 
+print (second_list *2)
+print (my_list + second_list) 
+
+
+my_tuple =[True, 786, 3.14, 'text', 70.2]
+second_tuple =[123, 'text']
+
+print (my_tuple) 
+print (my_tuple[0])
+print (my_tuple[1:3])
+print (my_tuple[2:]) 
+print (second_tuple *2)
+print (my_tuple + second_tuple) 
+
+
+my_dict = { }
+my_dict['country'] = 'Mexico'
+print (my_dict['country'])
+
+another_dict = {'number': 23, 2: True, 'my_list': [1,2,3]}
+print (another_dict.keys())
+print (another_dict.values())
+
+
+s = set()
+print (s)
+s = {'hi', 'bye'}
+print (s)
+
+
+
+
+
+
+
+
+
+
+
+

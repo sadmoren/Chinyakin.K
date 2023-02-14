945c1b44253fe301c7c00381f8c6d3e0c50d5076
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-02-14 11:31:31 +0300

    Add files via upload

---
+++ пр. 2.py
@@ -0,0 +1,15 @@
+x = 10
+y = 5
+x < 11 and y == 5
+x == 10 and y > 0
+x > 10 and y == 10
+x < 10 and y == 10
+
+x != 10 or y != 10
+x > 9 or y == 5 
+x == 9 or y == 0 
+x > 11 or y < 4 
+
+str1 = 'a'
+str2 = 'b'
+str1 < 'b' and str2 != 'a'

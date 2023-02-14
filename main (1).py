551d83ec99757286e70e43293efb6606113ad470
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-02-14 11:15:05 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = 'No'
+big_netseloe = tseloe * 3.5
+drobnoe -= 1
+tseloe/drobnoe
+big_netseloe/drobnoe
+stroka = 'No'*2 + '_' + 'Yes'*3
+
+print (drobnoe)
+print (big_netseloe)
+print (stroka)
+print (drobnoe)
+print (tseloe)

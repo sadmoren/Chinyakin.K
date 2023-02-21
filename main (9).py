0a6f565837f4abd3647248cd34fb5db7270816a5
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-02-21 12:11:21 +0300

    классная работа

---
+++ main (9).py
@@ -0,0 +1,21 @@
+x = 4
+y = 10 
+if x > y: 
+    z = x - y 
+elif x < y: 
+    z = x + y 
+else: z = x 
+    
+print (z)
+
+
+
+age = int(input("твой возраст?:" ))
+if age == 18: 
+    print ("Вау")
+elif age == 14: 
+    print ('Шкет')
+elif age == 25:
+    print ('Мощно')
+else: 
+    print ('ты Пришелец?')

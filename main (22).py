534b3c4f29aa1992483c2521b2659a2bb4baa7b9
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-03-07 11:32:30 +0300

    ДОмашка

---
+++ main (22).py
@@ -0,0 +1,52 @@
+from math import sqrt 
+
+a = float(input())
+b = float(input())
+h = float(input())
+P = sqrt(h**2+((a-b)/2)**2)
+print (P)
+
+
+m = int(input())
+V = int(input())
+p = m/V
+print(p)
+
+
+a = int(input())
+b = int(input())
+z = b-a
+n = a*b
+f = a-b
+k = a/b 
+print (z, n, f, k)
+
+
+a = int(input())
+b = int(input())
+if a > 0 and b > 0:
+    z = (a + b)/2
+    print (z)
+    
+    
+k = int(input())
+s = int(input())
+p = s/k
+print (print)
+
+a = float(input())
+b = float(input())
+c = float(input())
+S = a*b*c
+V = 2(a*b+a*c+c*b)
+print(S, V)
+
+
+
+
+
+
+
+
+
+

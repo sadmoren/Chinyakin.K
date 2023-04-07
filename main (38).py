72af5b079b7d693212f3ec350ebb693e08aad1b0
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-04-07 12:10:47 +0300

    llll

---
+++ main (38).py
@@ -0,0 +1,31 @@
+dum = 10 
+mill = 25.4
+while dum <= 22:
+    sant = dum * mill
+    print (sant)
+    dum +=1 
+
+kurs = int(input())
+dol = 1 
+while dol <= 20:
+    rub = dol * kurs
+    print(rub)
+    dol+=1
+
+e = int(input())    
+i = 1 
+while i <= 20:
+    e **= i
+    print(e)
+    i += 1 
+
+i = 1.1 
+while i <= 21.1:
+    print (i)
+    i+=1    
+
+i = 2.1 
+while i <= 3:
+    print (i)
+    i+=0.1
+

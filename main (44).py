b74831a8462ae3b7eec94d55e32fb79a2e6bfa4c
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-04-11 12:07:58 +0300

    ff

---
+++ main (44).py
@@ -0,0 +1,25 @@
+i=0 
+while i<20:
+    i=i+2 
+    print(i)
+
+i=-1
+while i<15:
+    i=i+2 
+    print(i)
+    
+i=2 
+while i<21:
+    print(i**0.5)
+    i=i+2
+i=2
+c=int(input("Цена"))
+while i<11:
+    print(c*i)
+    i=i+1
+
+a=int(input("Начало"))
+b=int(input("Конец"))
+while a<b:
+    print(a**0.5)
+    a=a+1

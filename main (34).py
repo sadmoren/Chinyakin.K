04e4ca40e581cea408a29de8ea0156ad9741b191
sadmoren <124670052+sadmoren@users.noreply.github.com>
2023-04-14 11:17:19 +0300

    14.4

---
+++ main (34).py
@@ -0,0 +1,36 @@
+p=int(input( ))
+i=0
+while i<p:
+    print(i**0.5)
+    i=i+1
+
+a=int(input( ))
+b=int(input( ))
+c=a+1
+while c<b:
+    print(c)
+    c=c+1    
+
+a=int(input( ))
+b=int(input( ))
+c=b-1
+while c>a:
+    print(c)
+    c=c-1
+    n=int(input( ))
+i=0
+while i<n:
+    print(i)
+    i=i+1
+
+    n=int(input(  ))
+k=int(input(  ))
+while 3*k>n:
+    print(k)
+    k=k-1 
+
+    n=int(input(  ))
+k=int(input(  ))
+while 3*k<n:
+    print(k)
+    k=k+1
